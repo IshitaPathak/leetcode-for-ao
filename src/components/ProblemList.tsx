af23cb837185e1ae2c5bd848160e43701d36e896
IshitaPathak <pathakishita111@gmail.com>
2025-07-20 18:43:34 +0530

    added dark mode

--- src/components/ProblemList.tsx
+++ src/components/ProblemList.tsx
@@ -10,7 +10,7 @@ type ProblemListProps = {
 	unlockedProblems: string[];
 };
 //testingggggg test againnnnn
-// testingggggggggggggggggggg
+// testinggggggggggggggggggggg
 
 export const ProblemList: React.FC<ProblemListProps> = ({
 	problems,
