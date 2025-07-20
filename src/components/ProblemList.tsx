28df71a0c13ea0cad6a325750779f6e9263097b1
IshitaPathak <pathakishita111@gmail.com>
2025-07-20 17:11:37 +0530

    added timer for each problem

--- src/components/ProblemList.tsx
+++ src/components/ProblemList.tsx
@@ -10,7 +10,7 @@ type ProblemListProps = {
 	unlockedProblems: string[];
 };
 //testingggggg test againnnn
-// testinggggggggggggg
+// testingggggggggggggg
 
 export const ProblemList: React.FC<ProblemListProps> = ({
 	problems,
